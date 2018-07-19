bee3bae001ff6802a8f0edf5aa69d9c90e62af24
Tom Chaudhry <tayyabji@comcast.net>
2018-07-18 23:10:23 -0400

    Added Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
